6d79e002f0ce77184f77f52dd8965b9a5e524094
Monayem_Sarker <75083786+MonayemSarker@users.noreply.github.com>
2024-07-27 23:32:58 +0600

    t end2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
-import Hello from "./components/Hello";
+import Fruits from "./components/Fruits";
+// import Hello from "./components/Hello";
 
 function App() {
-  const seatNumbers = [1, 4, 7];
+  // const seatNumbers = [1, 4, 7];
 
   return (
     <>
-      <Hello name="Miraj" seatNumbers={seatNumbers} />
-      <Hello name="Boby" />
-      <Hello name="Zahin" />
+      <Fruits />
     </>
   );
 }
--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,4 +1,6 @@
+import Fruit from "./Fruit";
+
 export default function Fruits() {
   const fruits = ["Apple", "Banana", "Guava"];
-  return <div>{fruits}</div>;
+  return <div> </div>;
 }
